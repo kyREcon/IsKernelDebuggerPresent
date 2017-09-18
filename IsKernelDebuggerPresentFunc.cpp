4aabcbd956c27d178082881f9507bfd66dd62c97
kyREcon <31939521+kyREcon@users.noreply.github.com>
2017-09-18 13:25:29 +0300

    Update IsKernelDebuggerPresentFunc.cpp

--- IsKernelDebuggerPresentFunc.cpp
+++ IsKernelDebuggerPresentFunc.cpp
@@ -6,7 +6,7 @@ int IsKernelDebuggerPresent()
 		if ((*(unsigned char*)(KUSER_SHARED_DATA_ADDRESS + KD_DEBUGGER_ENABLED_OFFSET) & 3) != 0)
 			return KERNEL_DEBUGGER_DETECTED;
 		else
-		return KERNEL_DEBUGGER_NOT_DETECTED;
+			return KERNEL_DEBUGGER_NOT_DETECTED;
 	}
 	__except (EXCEPTION_EXECUTE_HANDLER)
 	{
